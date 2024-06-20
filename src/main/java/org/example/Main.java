b802fd36387bc6eecac4f46d6532e49d65532273
AstorAlina <astoralina@gmail.com>
2024-06-20 14:13:00 -0500

    changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,2 @@
-package org.example;
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-
-        UserInterface userInterface = new UserInterface();
-        while (true) {
-            userInterface.init();
-            userInterface.display();
-        }
-
-
-    }
+package org.example;public class Main {
 }
